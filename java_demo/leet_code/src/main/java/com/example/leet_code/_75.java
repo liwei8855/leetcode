362cc80cd9e440eac4897f1907a1458c39590e8e
“weil” <weil@opera.com>
2021-06-07 12:43:26 +0800

    fix75颜色分类

--- java_demo/leet_code/src/main/java/com/example/leet_code/_75.java
+++ java_demo/leet_code/src/main/java/com/example/leet_code/_75.java
@@ -2,7 +2,8 @@ package com.example.leet_code;
 //颜色分类
 public class _75 {
     public static void main(String[] args) {
-        int[] arr = {2,0,2,1,1,0};
+//        int[] arr = {2,0,2,1,1,0};
+        int[] arr = {1,0,2};
         sortColors(arr);
         for (int i=0;i<arr.length;i++){
             System.out.print(arr[i]);
@@ -12,20 +13,29 @@ public class _75 {
     static void sortColors(int[] nums) {
         int begin = 0;
         int end = nums.length-1;
-        while (begin<end){
-            if (nums[begin]==2){
-                int tmp = nums[begin];
-                nums[begin] = nums[end];
-                nums[end] = tmp;
+        int index = begin;
+        while (index<=end){
+            if (nums[index]==2){
+                int tmp = nums[end];
+                nums[end] = nums[index];
+                nums[index] = tmp;
                 end--;
-            } else if(nums[end]==0){
+            } else if(nums[index]==0){
                 int tmp = nums[begin];
-                nums[begin]=nums[end];
-                nums[end]=tmp;
+                nums[begin]=nums[index];
+                nums[index]=tmp;
                 begin++;
+                index++;
             } else {
-                begin++;
+                index++;
             }
         }
+    }
+
+    static void pList(int[] list){
+        for (int i=0;i<list.length;i++){
+            System.out.print(list[i]+"_");
+        }
+        System.out.println();
     }
 }
